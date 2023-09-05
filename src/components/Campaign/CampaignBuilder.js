a5bcc33ba6c1af05e4255c14fc82d38965be9cbc
Just Eryk <justeryk@MBP-Damian-2.localdomain>
2023-09-05 10:46:14 +0200

    2

--- src/components/Campaign/CampaignBuilder.js
+++ src/components/Campaign/CampaignBuilder.js
@@ -4,45 +4,55 @@ import classes from "./Form.module.css";
 import Form from "./Form";
 
 const CampaignBuilder = () => {
-	const local = "local"
-	localStorage.setItem("local", local)
+	// const local = "local"
+	// localStorage.setItem("local", local)
 
-	function setCookie(name, value, days) {
-		const expirationDate = new Date();
-		expirationDate.setDate(expirationDate.getDate() + days);
+	// function setCookie(name, value, days) {
+	// 	const expirationDate = new Date();
+	// 	expirationDate.setDate(expirationDate.getDate() + days);
 
-		const cookieValue = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
-		document.cookie = cookieValue;
-	}
-	setCookie('cookie', 'ciasteczka', 7);
+	// 	const cookieValue = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
+	// 	document.cookie = cookieValue;
+	// }
+	// setCookie('cookie', 'ciasteczka', 7);
 
-	function getCookie(name) {
-		const cookies = document.cookie.split(';');
-		for (const cookie of cookies) {
-			const [cookieName, cookieValue] = cookie.trim().split('=');
-			if (cookieName === name) {
-				return cookieValue;
-			}
-		}
-		return null; // Zmienna nie znaleziona
-	}
+	// function getCookie(name) {
+	// 	const cookies = document.cookie.split(';');
+	// 	for (const cookie of cookies) {
+	// 		const [cookieName, cookieValue] = cookie.trim().split('=');
+	// 		if (cookieName === name) {
+	// 			return cookieValue;
+	// 		}
+	// 	}
+	// 	return null; // Zmienna nie znaleziona
+	// }
 
-	const wartośćZmiennej = getCookie('cookie');
-	if (wartośćZmiennej !== null) {
-		return (
-			<>
-				<section className={classes.campaign}>
-					<Form />
-				</section>
+	// const wartośćZmiennej = getCookie('cookie');
+	// if (wartośćZmiennej !== null) {
+	// 	return (
+	// 		<>
+	// 			<section className={classes.campaign}>
+	// 				<Form />
+	// 			</section>
 
-				<YourCampaign />
-			</>
-		);
-	} else {
-		return (
-			<div><p>Nie ma cookies</p></div>
-		)
-	}
+	// 			<YourCampaign />
+	// 		</>
+	// 	);
+	// } else {
+	// 	return (
+	// 		<div><p>Nie ma cookies</p></div>
+	// 	)
+	// }
+
+	return (
+		<>
+			<section className={classes.campaign}>
+				<Form />
+			</section>
+
+			<YourCampaign />
+		</>
+	);
 
 };
 
