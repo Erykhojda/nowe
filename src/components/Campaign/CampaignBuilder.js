ee4cb7e64cb892879bf655e8eee02712eab1e0de
Just Eryk <justeryk@MBP-Damian-2.localdomain>
2023-09-05 10:56:27 +0200

    cfcf

--- src/components/Campaign/CampaignBuilder.js
+++ src/components/Campaign/CampaignBuilder.js
@@ -4,57 +4,56 @@ import classes from "./Form.module.css";
 import Form from "./Form";
 
 const CampaignBuilder = () => {
-	const local = "local"
-	localStorage.setItem("local", local)
 
-	// function setCookie(name, value, days) {
-	// 	const expirationDate = new Date();
-	// 	expirationDate.setDate(expirationDate.getDate() + days);
 
-	// 	const cookieValue = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
-	// 	document.cookie = cookieValue;
-	// }
-	// setCookie('cookie', 'ciasteczka', 7);
+	function setCookie(name, value, days) {
+		const expirationDate = new Date();
+		expirationDate.setDate(expirationDate.getDate() + days);
 
-	// function getCookie(name) {
-	// 	const cookies = document.cookie.split(';');
-	// 	for (const cookie of cookies) {
-	// 		const [cookieName, cookieValue] = cookie.trim().split('=');
-	// 		if (cookieName === name) {
-	// 			return cookieValue;
-	// 		}
-	// 	}
-	// 	return null; // Zmienna nie znaleziona
-	// }
+		const cookieValue = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`;
+		document.cookie = cookieValue;
+	}
+	setCookie('cookie', 'ciasteczka', 7);
 
-	// const wartośćZmiennej = getCookie('cookie');
-	// if (wartośćZmiennej !== null) {
-	// 	return (
-	// 		<>
-	// 			<section className={classes.campaign}>
-	// 				<Form />
-	// 			</section>
+	function getCookie(name) {
+		const cookies = document.cookie.split(';');
+		for (const cookie of cookies) {
+			const [cookieName, cookieValue] = cookie.trim().split('=');
+			if (cookieName === name) {
+				return cookieValue;
+			}
+		}
+		return null; // Zmienna nie znaleziona
+	}
 
-	// 			<YourCampaign />
-	// 		</>
-	// 	);
-	// } else {
-	// 	return (
-	// 		<div><p>Nie ma cookies</p></div>
-	// 	)
-	// }
-	const localq = localStorage.getItem("local")
-	return (
-		<>
+	const wartośćZmiennej = getCookie('cookie');
+	if (wartośćZmiennej !== null) {
+		return (
+			<>
+				<section className={classes.campaign}>
+					<h1>{wartośćZmiennej}</h1>
+					<Form />
+				</section>
 
-			<section className={classes.campaign}>
-				<h1>{localq}</h1>
-				<Form />
-			</section>
+				<YourCampaign />
+			</>
+		);
+	} else {
+		return (
+			<div><p>Nie ma cookies</p></div>
+		)
+	}
+	// return (
+	// 	<>
 
-			<YourCampaign />
-		</>
-	);
+	// 		<section className={classes.campaign}>
+	// 			<h1>{localq}</h1>
+	// 			<Form />
+	// 		</section>
+
+	// 		<YourCampaign />
+	// 	</>
+	// );
 
 };
 
